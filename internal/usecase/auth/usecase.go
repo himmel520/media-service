6bf70e4358846b8cf070808dad977efa0f5ef57a
Himmel <122121613+himmel520@users.noreply.github.com>
2024-12-07 23:28:36 +0200

    end auth (#62)
    Co-authored-by: m <mvmarina063@gmail.com>

--- internal/usecase/auth/usecase.go
+++ internal/usecase/auth/usecase.go
@@ -1,20 +1,12 @@
 package authUC
 
 import (
-	"context"
 	"crypto/rsa"
-
-	"github.com/himmel520/media-service/internal/infrastructure/repository"
 )
 
 type (
 	AuthUC struct {
 		publicKey rsa.PublicKey
-	}
-
-	DBXT interface {
-		DB() repository.Querier
-		InTransaction(ctx context.Context, fn repository.TransactionFunc) error
 	}
 )
 
